92ba94162762e97c139bc97d7d13b979b81ab6f4
Reyhaneh Seraj <63651494+rseraj@users.noreply.github.com>
2023-06-26 00:35:34 +0330

    refactor codebase

---
+++ Gold-Miner-Project/Assets/Scripts/Player Scripts/PlayerAnimation.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerAnimation : MonoBehaviour
+{
+    private Animator anim;
+
+    void Awake()
+    {
+        anim = GetComponent<Animator>();
+    }
+
+    public void IdleAnimation()
+    {
+        anim.Play(AnimationTags.IDLE_ANIMATION);
+    }
+
+    public void PullingItemAnimation()
+    {
+        anim.Play(AnimationTags.ROPE_WRAP_ANIMATION);
+    }
+    public void ChearAnimation()
+    {
+        anim.Play(AnimationTags.CHEAR_ANIMATION);
+    }
+
+
+}
+
+
